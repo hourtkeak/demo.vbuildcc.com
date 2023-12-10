c7f81d30a1679c303ec8ba9df7a44a44fa6e6272
hourtkeak1 <hourtkeak@gmail.com>
2023-12-10 23:28:35 +0700

    test-a

--- index.php
+++ index.php
@@ -190,16 +190,165 @@ Architectural solutions, custom-fit for your <br> <span class="text-accent-color
                                 </div><!-- /.col-md-4 -->
                             </div>
                         </div>
-                    </section>                    
+                    </section>   
+                    <!-- WORKS -->
+                    <section class="wprt-section works parallax">
+                        <div class="container">
+                            <div class="row">
+                                <div class="col-md-12">
+                                    <div class="wprt-spacer" data-desktop="70" data-mobi="60" data-smobi="60"></div>
+                                    <h2 class="text-left">FEATURED WORK</h2>
+                                    <div class="wprt-lines custom-2">
+                                        <div class="line-1"></div>
+                                    </div>
+                                    <div class="wprt-spacer" data-desktop="50" data-mobi="40" data-smobi="40"></div>
+                                </div><!-- /.col-md-12 -->
+                                        
+                                <div class="col-md-12">
+                                    <div class="wprt-project arrow-style-2 has-arrows arrow60 arrow-dark" data-layout="slider" data-column="3" data-column2="3" data-column3="2" data-column4="1" data-gaph="30" data-gapv="30">
+                                        <div id="projects" class="cbp">
+                                            <div class="cbp-item">
+                                                <div class="project-item">
+                                                    <div class="inner">
+                                                        <div class="grid">
+                                                        <figure class="effect-sadie">
+                                                            <img src="assets/img/projects/2.jpg" alt="image" />
+                                                            <figcaption>
+                                                                <div>
+                                                                    <h2><a target="_blank" href="page-project-detail.html">LUXURY BUILDINGS</a></h2>
+                                                                    <p>Construction</p>
+                                                                </div>
+                                                            </figcaption>           
+                                                        </figure>
+                                                        </div>
+
+                                                        <a class="project-zoom cbp-lightbox" href="assets/img/projects/1-full.jpg" data-title="LUXURY BUILDINGS">
+                                                            <i class="fa fa-arrows-alt"></i>
+                                                        </a>
+                                                    </div>
+                                                </div>
+                                            </div><!--/.cbp-item -->
+
+                                            <div class="cbp-item">
+                                                <div class="project-item">
+                                                    <div class="inner">
+                                                        <figure class="effect-honey">
+                                                            <img src="assets/img/projects/2.jpg" alt="image" />
+                                                            <figcaption>
+                                                                <div>
+                                                                    <h2><a target="_blank" href="page-project-detail-2.html">LUXURY BUILDINGS</a></h2>
+                                                                    <p>Construction</p>
+                                                                </div>
+                                                            </figcaption>           
+                                                        </figure>
+
+                                                        <a class="project-zoom cbp-lightbox" href="assets/img/projects/2-full.jpg" data-title="LUXURY BUILDINGS">
+                                                            <i class="fa fa-arrows-alt"></i>
+                                                        </a>
+                                                    </div>
+                                                </div>
+                                            </div><!--/.cbp-item -->
+
+                                            <div class="cbp-item">
+                                                <div class="project-item">
+                                                    <div class="inner">
+                                                        <figure class="effect-sadie">
+                                                            <img src="assets/img/projects/3.jpg" alt="image" />
+                                                            <figcaption>
+                                                                <div>
+                                                                    <h2><a target="_blank" href="page-project-detail-3.html">LUXURY BUILDINGS</a></h2>
+                                                                    <p>Construction</p>
+                                                                </div>
+                                                            </figcaption>           
+                                                        </figure>
+
+                                                        <a class="project-zoom cbp-lightbox" href="assets/img/projects/3-full.jpg" data-title="LUXURY BUILDINGS">
+                                                            <i class="fa fa-arrows-alt"></i>
+                                                        </a>
+                                                    </div>
+                                                </div>
+                                            </div><!--/.cbp-item -->
+
+                                            <div class="cbp-item">
+                                                <div class="project-item">
+                                                    <div class="inner">
+                                                        <figure class="effect-zoe">
+                                                            <img src="assets/img/projects/2.jpg" alt="image" />
+                                                            <figcaption>
+                                                                <div>
+                                                                    <h2><a target="_blank" href="page-project-detail-4.html">LUXURY BUILDINGS</a></h2>
+                                                                    <p>Construction</p>
+                                                                </div>
+                                                            </figcaption>           
+                                                        </figure>
+
+                                                        <a class="project-zoom cbp-lightbox" href="assets/img/projects/1-full.jpg" data-title="LUXURY BUILDINGS">
+                                                            <i class="fa fa-arrows-alt"></i>
+                                                        </a>
+                                                    </div>
+                                                </div>
+                                            </div><!--/.cbp-item -->
+
+                                            <div class="cbp-item">
+                                                <div class="project-item">
+                                                    <div class="inner">
+                                                        <figure class="effect-oscar">
+                                                            <img src="assets/img/projects/2.jpg" alt="image" />
+                                                            <figcaption>
+                                                                <div>
+                                                                    <h2><a target="_blank" href="page-project-detail.html">LUXURY BUILDINGS</a></h2>
+                                                                    <p>Construction</p>
+                                                                </div>
+                                                            </figcaption>           
+                                                        </figure>
+
+                                                        <a class="project-zoom cbp-lightbox" href="assets/img/projects/2-full.jpg" data-title="LUXURY BUILDINGS">
+                                                            <i class="fa fa-arrows-alt"></i>
+                                                        </a>
+                                                    </div>
+                                                </div>
+                                            </div><!--/.cbp-item -->
+
+                                            <div class="cbp-item">
+                                                <div class="project-item">
+                                                    <div class="inner">
+                                                        <figure class="effect-sadie">
+                                                            <img src="assets/img/projects/3.jpg" alt="image" />
+                                                            <figcaption>
+                                                                <div>
+                                                                    <h2><a target="_blank" href="page-project-detail-2.html">LUXURY BUILDINGS</a></h2>
+                                                                    <p>Construction</p>
+                                                                </div>
+                                                            </figcaption>           
+                                                        </figure>
+
+                                                        <a class="project-zoom cbp-lightbox" href="assets/img/projects/3-full.jpg" data-title="LUXURY BUILDINGS">
+                                                            <i class="fa fa-arrows-alt"></i>
+                                                        </a>
+                                                    </div>
+                                                </div>
+                                            </div><!--/.cbp-item -->
+                                        </div><!-- /#projects -->
+                                    </div><!--/.wprt-project -->
+                                </div><!-- /.col-md-12 -->
+
+                                <div class="col-md-12">
+                                    <div class="wprt-spacer" data-desktop="80" data-mobi="60" data-smobi="60"></div>
+                                </div><!-- /.col-md-12 -->
+                            </div><!-- /.row -->
+                        </div><!-- /.container -->
+                    </section>
+
                     <!-- SERVICES -->
                     <section id="services-section" class="wprt-section services-2">
                         <div class="container">
                             <div class="row">
                                 <div class="col-md-12">
                                     <div class="wprt-spacer" data-desktop="70" data-mobi="60" data-smobi="60"></div>
-                                    <h2 class="text-center margin-bottom-10">OUR SERVICES</h2>
-                                    <div class="wprt-lines style-2 custom-1">
+                                    <h2>Services</h2>
+                                    <div class="wprt-lines style-1 custom-3">
                                         <div class="line-1"></div>
+                                        <div class="line-2"></div>
                                     </div>
 
                                     <div class="wprt-spacer" data-desktop="32" data-mobi="32" data-smobi="32"></div>
